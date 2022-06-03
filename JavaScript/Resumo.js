a1090481ceced16d916af314f3f8cfa34777bad0
“Brunofranca83” <Brunofranca83@yahoo.com.br>
2022-06-02 22:59:34 -0300

    Resumo JavaScript

---
+++ JavaScript/Resumo.js
@@ -0,0 +1,23 @@
+/*Java Script
+
+è uma linguagem interpretada (O Browser roda em tempo real e o código é disponibilizado instantaneamente). - Baseada em protótipos
+
+Multiparadigma: Você pode escolher ser vc quer voltar o JS para objetos, ou páginas etc
+
+Comumente utilizada em aplicações Web client-side: a parte que interage com o usário
+
+Segue o padrão ECMAScript: (Um Padrão para várias Linguagens)*/
+
+
+/*Funções
+
+Sintaxe para declarar:*/
+
+function soma(a, b){ //Esse parenteses serve pr declarar parametros
+    console.log(a+b); 
+    return a+b; //Isso é usado quando vc precisa usar o resultado dessa função em outro lugar
+}
+
+/*function soma (parametros) {}*/
+
+

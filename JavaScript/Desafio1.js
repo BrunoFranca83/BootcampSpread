fe3a62a76e5fdc2fc96b574c9555bba1aa5d4c77
“Brunofranca83” <Brunofranca83@yahoo.com.br>
2022-06-16 23:44:38 -0300

    Desafio

---
+++ JavaScript/Desafio1.js
@@ -0,0 +1,9 @@
+//Vamos ao desafio 1 do bootcamp da Spread. 
+//A idéia é fazer uma soma simples. Here we go!
+
+function multiplica(a, b){
+    let resultado = 2 * 7;
+    return resultado;
+}
+
+console.log(resultado)
